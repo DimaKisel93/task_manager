c7833ee84e5540a056b9278478241c78dc6532dc
Dzmitry Kisel <dimakisel333@gmail.com>
2026-04-24 16:12:38 +0300

    feat: add router and mui providers

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material'
+import { Link, Navigate, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <Box sx={{ minHeight: '100vh', backgroundColor: '#f5f7fb' }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Task Manager
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Tasks
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="md" sx={{ py: 4 }}>
+        <Routes>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Container>
+    </Box>
+  )
+}
+
+export default App
