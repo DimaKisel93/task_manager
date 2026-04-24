17f32e3dc64e71dfdfd369525dfc2bb3415beb7e
Dzmitry Kisel <dimakisel333@gmail.com>
2026-04-24 16:46:24 +0300

    feat: configure redux store with rtk query middleware

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-
+import { store } from './app/store'
 import './index.css'
 import App from './App.tsx'
 
@@ -10,16 +11,18 @@ const theme = createTheme()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
